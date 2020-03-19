620b4e412b4f6bb3bc2f5f2a24e3e2e10e8b46f1
TheTerabit <terrabitroman@gmail.com>
2020-03-19 14:11:17 +0100

    initial commit

---
+++ src/main/java/pl/bs/paintmysite/PaintMySiteApplication.java
@@ -0,0 +1,13 @@
+package pl.bs.paintmysite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintMySiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintMySiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bs/paintmysite/PaintMySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bs.paintmysite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintMySiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
